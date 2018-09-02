7ea4997b8ed5c18a5c207da50e529321390e5795
Pablo Muguerza <pmuguer@gmail.com>
2018-09-02 20:24:07 -0300

    Se había subido por error una respuesta HTTP 400; corregido

--- example_app/snippets/views.py
+++ example_app/snippets/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework import status
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from snippets.models import Snippet
@@ -41,7 +40,7 @@ class SnippetViewSet(viewsets.ModelViewSet):
         return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
