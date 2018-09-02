26a5208c5a6551e6eec5ac68d88565b89a9cdbc1
Pablo Muguerza <pmuguer@gmail.com>
2018-09-02 20:28:21 -0300

    Agregada configuración para acceder a la documentación 'built-in' de DRF

--- example_app/tutorial/urls.py
+++ example_app/tutorial/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 from rest_framework_swagger.views import get_swagger_view
 
@@ -14,5 +15,6 @@ schema_view = get_swagger_view(title='Snippets API')
 urlpatterns = [
     url('^$', schema_view),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include_docs_urls(title='My API title'))
 ]
